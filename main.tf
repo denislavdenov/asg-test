2d26a85d9d09a7fe6c91a41be90302631f3ff43b
Denislav Denov <denislavdenov@gmail.com>
2020-08-31 08:27:07 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ resource "aws_launch_template" "foobar" {
 
 resource "aws_autoscaling_group" "bar" {
   availability_zones = ["us-east-1a"]
-  desired_capacity   = 0
+  desired_capacity   = 1
   max_size           = 2
-  min_size           = 0
+  min_size           = 1
 
   launch_template {
     id      = aws_launch_template.foobar.id
